a329fab4e7787062db578106506ada89b3b76bc2
seongkyun <yooss135@naver.com>
2024-12-12 18:33:01 +0900

    git init

---
+++ src/test/java/com/example/matdil/MatDilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matdil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatDilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
